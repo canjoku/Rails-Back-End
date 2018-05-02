684908dfdb1b072f4b1999825a9e73be8047349f
Tony <ca.njoku@yahoo.com>
2018-05-02 06:18:32 +0100

    minor edits. Wait for review

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.paginate(:page => params[:page], :per_page => 6)
   end
 
   def show
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
 
