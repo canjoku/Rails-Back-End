a9fca661277dae68adbeca5a370948ab1a1c10d8
Tony <ca.njoku@yahoo.com>
2018-05-02 05:03:02 +0100

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :posts, only: [:index, :show]
-  resources :users, except: [:destroy]
+  resources :users, except: [:destroy, :new]
+
+  get 'signup' => 'users#new', as: :signup
   get 'contact' => 'pages#contact', as: :contact
 
   namespace :admin do
