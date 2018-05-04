5df366b1993ac81fccf1170193f623183d06a01d
Tony <ca.njoku@yahoo.com>
2018-05-04 05:58:40 +0100

    review changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
+
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     "https://secure.gravatar.com/avatar/#{gravatar_id}.png"
   end
+
 end
