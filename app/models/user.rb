31334192ea5fac9681ff47dc58eb3a23c5276070
Tony <ca.njoku@yahoo.com>
2018-05-10 05:25:32 +0100

    post model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     length: { minimum: 2 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:true, length: {maximum: 30},
+  validates :email, presence:true,
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
 
