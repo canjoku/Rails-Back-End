e9f0579f3870dfd71948180620a083b919aa9fab
Tony <ca.njoku@yahoo.com>
2018-05-18 05:12:43 +0100

    started working on forms

--- app/javascript/hamburger.js
+++ app/javascript/hamburger.js
@@ -1,5 +1,5 @@
 export default window.onload = function () {
-  const navHamburger = document.getElementById('navHamburger')
+  const navHamburger = document.querySelector('[data-attribute="navHamburger"]')
   navHamburger.addEventListener('click', function (event) {
     event.preventDefault()
     document.querySelector('.c-nav__toggle').classList.toggle('c-nav-active__toggle')
