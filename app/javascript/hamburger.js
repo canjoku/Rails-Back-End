e24907aff49f718614285249f27b103391f129d4
Tony <ca.njoku@yahoo.com>
2018-05-22 13:59:42 +0100

    review corections

--- app/javascript/hamburger.js
+++ app/javascript/hamburger.js
@@ -2,7 +2,7 @@ export default window.onload = function () {
   const navHamburger = document.querySelector('[data-attribute="navHamburger"]')
   navHamburger.addEventListener('click', function (event) {
     event.preventDefault()
-    document.querySelector('.c-nav__toggle').classList.toggle('c-nav-active__toggle')
-    document.querySelector('.c-nav').classList.toggle('c-nav-active')
+    document.querySelector('[data-attribute="navHamburger"]').classList.toggle('c-nav-active__toggle')
+    document.querySelector('[data-attribute="navHamburger-wrapper"]').classList.toggle('c-nav-active')
   })
 }
