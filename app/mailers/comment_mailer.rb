3bb24613825db4cb0bf7f2b99429675ddfe4ede6
Tony Njoku <ca.njoku@yahoo.com>
2019-01-10 01:18:35 +0000

    Updates/postmark (#32) (#33)
    * postmark set up. old mailers removed
    
    * configured postmark
    
    * updated gem file

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,6 +1,6 @@
 class CommentMailer < ApplicationMailer
   def new_comment_email(comment_id)
     @comment = Comment.find(comment_id)
-    mail(to: ENV.fetch("ADMIN_EMAIL"), subject: "A new comment has been posted")
+    mail(to: ENV.fetch('ADMIN_EMAIL'), subject: "A new comment has been posted")
   end
 end
