66c2ed320e9cfb0aa8a59a174c61c1f0660b5843
Tony <ca.njoku@yahoo.com>
2019-01-16 00:20:51 +0000

    added image retrieval to graphql

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,9 +5,21 @@ class Types::PostType < Types::BaseObject
   field :title, String, null: false
   field :body, String, null: false
   field :comments, [Types::CommentType], null: true
+  field :created_at, String, null: false
+  field :slug, String, null: false
+  field :avatar_url, String, null: false
+  field :avatar_thumbnail, String, null: false
 
   def comments
     object.comments
+  end
+
+  def avatar_url
+    Rails.application.routes.url_helpers.rails_blob_url(object.avatar, only_path: true)
+  end
+
+  def avatar_thumbnail
+    Rails.application.routes.url_helpers.polymorphic_url(object.avatar_thumbnail, only_path: true)
   end
 
 end
