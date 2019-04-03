6ede62cfc6cdb5f2920437380e963e07503083b9
apple <apple@apples-mbp.home>
2019-04-03 20:29:29 +0100

    updated app controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  protect_from_forgery with: :null_session
   before_action :authenticate
 
   protected
